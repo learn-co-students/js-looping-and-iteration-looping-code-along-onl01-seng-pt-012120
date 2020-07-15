92f2e4d34a5535a35a4ffcaa3f90a80986247fa6
d-otis <dan@dan-foley.com>
2020-07-15 19:20:58 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+
+function writeCards(names, event) {
+  let cards = [];
+  for (let i = 0; i < names.length; i++ ) {
+    cards.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`)
+  }
+
+  return cards;
+};
+
+// needs to return2
+// [
+//   "Thank you, Ada, for the wonderful birthday gift!",
+//   "Thank you, Brendan, for the wonderful birthday gift!",
+//   "Thank you, Ali, for the wonderful birthday gift!"
+// ]
+
+// writeCards(["Ada", "Brendan", "Ali"], "birthday");
+
+function countDown(num) {
+  while (num >= 0) {
+    console.log(num);
+    num--
+  };
+};
