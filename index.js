6a0e8a7d5b1d7adf8cddadd96297fd27c1f918eb
github username <github email address>
2020-07-17 13:38:44 -0700

    Done.

--- index.js
+++ index.js
@@ -5,33 +5,31 @@
 
 
   const cards = (["Ada", "Brendan", "Ali"], "birthday");
-  
- cards.concat
- cards.pop()
-function writeCards(cards) {
+ cards.concat;
+ cards.pop();
+
+function writeCards(cards) 
+{
+const newmessage = [];
+
   for (let i = 0; i < cards.length; i++) {
-    console.log(`Thank you, ${cards[i]} , for the wonderful birthday gift!`);
+    console.log(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
+    newmessage.push(`Thank you, ${cards[i]}, for the wonderful surprise gift!`);
   }
  
-  return cards;
+  return newmessage;
 }
+ writeCards;
 
-
-
+// 2) index.js
+//        countDown()
+//          logs each number when counting down, starting from the number provided:
+  
 
 function countDown() {
-for (var  countDown = 11; countDown > 0; countDown--) {
+for (var  countDown = 10; countDown >= 0; countDown--) {
   console.log(countDown);
 }
 }
 countDown();
 
-// 2) index.js
-//        countDown()
-//          logs each number when counting down, starting from the number provided:
-    function Down() {
-  for (var Down = 4; Down >= 0; Down--) {
-    console.log(Down);
-  }
-   }
-  Down();
