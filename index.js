514351c045a557fc01d34101f2c42f3004734466
github username <github email address>
2020-07-17 12:39:22 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solutions in this file
+
+// writeCards()
+// returns an array of thank you messages for each name provided to the function:
+
+
+  const cards = (["Ada", "Brendan", "Ali"], "birthday");
+  
+ cards.concat
+ cards.pop()
+function writeCards(cards) {
+  for (let i = 0; i < cards.length; i++) {
+    console.log(`Thank you, ${cards[i]} , for the wonderful birthday gift!`);
+  }
+ 
+  return cards;
+}
+
+
+
+
+function countDown() {
+for (var  countDown = 11; countDown > 0; countDown--) {
+  console.log(countDown);
+}
+}
+countDown();
+
+// 2) index.js
+//        countDown()
+//          logs each number when counting down, starting from the number provided:
+    function Down() {
+  for (var Down = 4; Down >= 0; Down--) {
+    console.log(Down);
+  }
+   }
+  Down();
