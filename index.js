86c77a1359c9fe9529e893ed8c09524f18acdf32
James Rogers <jrogers.dev@gmail.com>
2020-07-22 00:56:05 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(names, event) {
+  let returnArray = []
+  for(let i = 0; i < names.length; i++) {
+    returnArray.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+  }
+  
+  return returnArray;
+}
+
+function countDown(number) {
+  for(let i = number; i >= 0; i--) {
+    console.log(i);
+  }
+}
