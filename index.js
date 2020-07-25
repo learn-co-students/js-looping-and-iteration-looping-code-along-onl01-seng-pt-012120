e318d80a66cfad12aba380a031e2d02de8d1b770
haileybongo <haileyebongo@gmail.com>
2020-07-25 12:46:49 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+let messages = []
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++){
+    messages.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+
+    return messages
+}
+
+function countDown(int) {
+    let i = int;
+    while ( i >= 0) {
+        console.log(i--);
+    }
+}
+
