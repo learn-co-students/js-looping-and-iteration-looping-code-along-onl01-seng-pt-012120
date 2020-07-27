86fa417e179459e6e2f382f331fa1d912fcc5470
LeonorPDX <leonoracolbert@gmail.com>
2020-07-27 10:58:31 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+
+function writeCards(names, event) {
+    let arr = []
+    for (let i = 0; i < names.length; i++) {
+        arr.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return arr;
+}
+
+function countDown(n) {
+    let countdown = n;
+    while(countdown >= 0) {
+        console.log(countdown--);
+    }
+}
