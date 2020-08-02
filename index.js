67b5faafab183ad0355428d319cd13ea5d2f312a
hmcdowell95 <hmcdowell95@github.com>
2020-08-02 15:55:34 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+const cards = ["teddy bear", "drone", "doll"];
+ 
+function writeCards(cards, holiday) {
+    let thankyous = [];
+  for (let i = 0; i < cards.length; i++) {
+    thankyous.push(`Thank you, ${cards[i]}, for the wonderful ${holiday} gift!`);
+  }
+  return thankyous;
+}
+ 
+function countDown(num) {
+    while (num != -1) {
+    console.log(num)
+    num = num - 1 }
+}
