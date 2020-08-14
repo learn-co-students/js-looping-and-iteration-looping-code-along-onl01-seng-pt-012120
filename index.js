b8cbe04a1f8075e622c8d57d6cd94d53b8b9db76
cpach96 <cpach96@gmail.com>
2020-08-14 10:24:49 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+
+    function writeCards(cards,event) {
+        let successMessages = [];
+        for (let i = 0; i < cards.length; i++) {
+
+            successMessages.push(`Thank you, ${cards[i]}, for the wonderful ${event} gift!`);
+
+        } 
+        return successMessages;
+    }
+    
+   
+    function countDown(number){
+        while (number >= 0) {
+            console.log(number--);
+        }
+        
+    }
