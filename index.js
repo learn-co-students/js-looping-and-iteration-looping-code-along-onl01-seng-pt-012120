f7958f67b0ebe9789c6e0f603ac2d07d9a2b4359
vanessuniq <vanessuniq@gmail.com>
2020-08-14 10:47:52 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 // Code your solutions in this file
+function writeCards(nameList, eventName) {
+    let messages = [];
+    for (let index = 0; index < nameList.length; index++) {
+        messages.push(`Thank you, ${nameList[index]}, for the wonderful ${eventName} gift!`);
+        
+    };
+    return messages;
+};
+
+function countDown(num) {
+    while (num >= 0) {
+        console.log(num--);
+    };
+};
